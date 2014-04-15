b895e19f818a4866b4828894a3700f7d066790f5
sadak pramodh <sadakpramodh@live.com>
2014-04-15 21:31:32 +0530

    Create configuration.php
    it is configuration file for all the site.

---
+++ configuration.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+<?php 
+$rights_name="sadakpramodh";
+$version="2.0.0";
+$year="2014";
+$your_website_link="http://sadakpramodh.tumblr.com";
+$administrator_name="sadakpramodh";
+/*$db = "database2";
+ $link = mysql_connect("localhost", "root", "");*/
+ /* for slam book only*/
+ $database="database2";
+ $host="localhost";
+ $database_username="root";
+ $database_password="";
+/* for user registration */
+$database_registration="database1";
+$host_registration="localhost";
+$username_db_registaration="root";
+$password_db_registration="";
+$table_registration="table1";
+
+
+?>
+</body>
+</html>
